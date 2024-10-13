2e708965a7ed9796b4bae0de0bc3dee4d18f43f5
yuuya-hoshino <yuuya-hoshino@github.com>
2024-10-14 02:01:46 +0900

    永続ログイン追加

--- src/src/context/AuthContext.tsx
+++ src/src/context/AuthContext.tsx
@@ -14,14 +14,23 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 // 認証状態を提供するコンポーネント
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [role, setRole] = useState<Role>(null); // 権限を管理
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(() => {
+    // ページロード時に localStorage から認証状態を取得
+    const savedAuthState = localStorage.getItem('isAuthenticated');
+    return savedAuthState === 'true';
+  });
+  
+  const [role, setRole] = useState<Role>(() => {
+    // ページロード時に localStorage からロール情報を取得
+    const savedRole = localStorage.getItem('role');
+    return savedRole === 'admin' || savedRole === 'general' ? savedRole : null;
+  });
 
   const login = (userRole: Role) => {
     setIsAuthenticated(true);
     setRole(userRole);
     localStorage.setItem('isAuthenticated', 'true');
-    localStorage.setItem('role', userRole ?? ''); // roleがnullの場合には空文字列を保存
+    localStorage.setItem('role', userRole ?? ''); // role が null の場合には空文字列を保存
   };
 
   const logout = () => {
